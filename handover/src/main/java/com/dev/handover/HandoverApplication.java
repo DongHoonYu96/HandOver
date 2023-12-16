780fd61785fb612fff0c33cb9818e38039b33fe2
DongHoonYu96 <dbehdgns71@naver.com>
2023-12-16 21:05:38 +0900

    feat : initial commit

---
+++ handover/src/main/java/com/dev/handover/HandoverApplication.java
@@ -0,0 +1,13 @@
+package com.dev.handover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandoverApplication.class, args);
+	}
+
+}
